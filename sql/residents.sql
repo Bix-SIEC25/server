bf858444969a557d2da0fae0b24f808658c30325
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-12-08 23:26:35 +0100

    chore: changing sql structure of residents (more info)

---
+++ sql/residents.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 08, 2025 at 11:21 PM
+-- Server version: 10.11.14-MariaDB
+-- PHP Version: 8.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `insa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `residents`
+--
+
+CREATE TABLE `residents` (
+  `device_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `notes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `residents`
+--
+
+INSERT INTO `residents` (`device_id`, `name`, `last_name`, `age`, `notes`) VALUES
+(1, 'Sammy', 'H.', 92, 'Osteoporosis'),
+(2, 'Valentin', 'S.', 72, 'Alzheimer'),
+(3, 'Batiste', 'M.', 65, 'Dementia'),
+(4, 'Hugo', 'R.', 88, 'Urinary incontinence'),
+(5, 'Gaspar', 'S.', 69, 'Sleep disorders'),
+(6, 'Cornelius', 'S.', 103, 'Cardiovascular diseases'),
+(7, 'Giovanna', 'G.', 0, 'Respiratory problems');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `residents`
+--
+ALTER TABLE `residents`
+  ADD PRIMARY KEY (`device_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
