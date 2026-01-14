11486f177167bbee90d9818132e673dc9a053504
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2026-01-14 14:47:57 +0100

    chore: adding placeholder for state machine

---
+++ add_state.php
@@ -0,0 +1,95 @@
+<?php
+
+if (!isset(
+    $_REQUEST["x"],
+    $_REQUEST["y"],
+    $_REQUEST["dir"],
+    $_REQUEST["wait_car"],
+    $_REQUEST["qr"],
+    $_REQUEST["face"],
+    $_REQUEST["dialog"],
+    $_REQUEST["fall_ia"],
+    $_REQUEST["mov_car"],
+    $_REQUEST["wait_image_verif"],
+)) {
+    header("Location: ./ko");
+    exit;
+}
+
+$x = htmlspecialchars($_REQUEST["x"]);
+$y = htmlspecialchars($_REQUEST["y"]);
+$dir = htmlspecialchars($_REQUEST["dir"]);
+$wait_car = htmlspecialchars($_REQUEST["wait_car"]);
+$qr = htmlspecialchars($_REQUEST["qr"]);
+$face = htmlspecialchars($_REQUEST["face"]);
+$dialog = htmlspecialchars($_REQUEST["dialog"]);
+$fall_ia = htmlspecialchars($_REQUEST["fall_ia"]);
+$mov_car = htmlspecialchars($_REQUEST["mov_car"]);
+$wait_image_verif = htmlspecialchars($_REQUEST["wait_image_verif"]);
+
+echo "Received: ".
+"<br>x=" . $x .
+"<br>y=" . $y .
+"<br>dir=" . $dir .
+"<br>wait_car=" . $wait_car .
+"<br>qr=" . $qr .
+"<br>face=" . $face .
+"<br>dialog=" . $dialog .
+"<br>fall_ia=" . $fall_ia .
+"<br>mov_car=" . $mov_car .
+"<br>wait_image_verif=" . $wait_image_verif;
+
+// https://bix.ovh/add_state.php?x=4&y=2&dir=1.5&wait_car=1&qr=0&face=0&dialog=0&fall_ia=0&mov_car=0&wait_image_verif=0
+// POST request supported too
+
+include_once("../insa_db.php");
+$db = dbConnect();
+
+// $sqlQuery = 'INSERT INTO bix_logs(sender, type, message) VALUES (:sender, :type, :msg)';
+
+// $sender = htmlspecialchars($_REQUEST["sender"]);
+// $type = htmlspecialchars($_REQUEST["type"]);
+// $msg = htmlspecialchars($_REQUEST["msg"]);
+
+// $insert = $db->prepare($sqlQuery);
+// $insert->execute([
+//     'sender' => $sender,
+//     'type' => $type,
+//     'msg' => $msg
+// ]);
+
+// if ($sender == "FaceRecognitionNode" || $sender == "QRNode") {
+//     copy("uploads/last.jpg", "uploads/visu_fall.jpg");
+//     @file_get_contents(
+//         "http://127.0.0.1:6442/push",
+//         false,
+//         stream_context_create(['http' => [
+//             'method' => 'POST',
+//             'header' => "Content-Type: text/plain\r\n",
+//             'content' => "bix/admin:visu_fall|$msg"
+//         ]])
+//     );
+// }
+
+// @file_get_contents(
+//     "http://127.0.0.1:6442/push",
+//     false,
+//     stream_context_create(['http' => [
+//         'method' => 'POST',
+//         'header' => "Content-Type: text/plain\r\n",
+//         'content' => "bix/logs:new log"
+//     ]])
+// );
+
+// @file_get_contents(
+//     "http://127.0.0.1:6442/push",
+//     false,
+//     stream_context_create(['http' => [
+//         'method' => 'POST',
+//         'header' => "Content-Type: text/plain\r\n",
+//         'content' => "bix/log_" . $sender . ":" . $type . "|" . $msg
+//     ]])
+// );
+
+header("Location: ./ok");
+exit;
